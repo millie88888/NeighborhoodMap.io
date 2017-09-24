42917eaadb0c15eed88ab1bf8269ac44a814e9d0
Jialing Liu <millie88888@gmail.com>
2017-09-23 23:26:48 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,53 +1,79 @@
-function fMarker(name, lat, long, type, content) {
-    this.name = ko.observable(name);
-    this.lat = ko.observable(lat);
-    this.long = ko.observable(long);
+// fMarker to set the marker display
+function fMarker(name, lat, lng, type, content) {
+    var icon = {
+        path: 'M24,0C14.2,0,6.3,7.8,6.3,17.4C6.3,32.9,24,48,24,48s17.7-15.4,17.7-30.6C41.7,7.8,33.8,0,24,0z',
+        fillColor: '#efb7c2',
+        fillOpacity: 1,
+        scale: .5,
+        strokeWeight: 2,
+        strokeColor: '#efb7c2',
+    };
+
+    this.name = name;
+    this.lat = lat;
+    this.long = lng;
     this.type = type;
     this.content = content;
+
     this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat, long),
+        position: new google.maps.LatLng(lat, lng),
         title: name,
         map: map,
         content: content,
-        draggable: true
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        icon: icon
+
     });
 
     this.streetViewService = new google.maps.StreetViewService();
     this.radius = 50;
 
 
+
     this.marker.addListener('click', function() {
         infowindow.setContent('<div><strong>' + this.title + '</strong></div>' + '<div>' + this.content + '</div><div id="pano"></div>');
         infowindow.open(map, this);
     });
-
-
-
 };
-
-
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: new google.maps.LatLng(38.9926756, -77.02641129999999),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-
-
-var infowindow = new google.maps.InfoWindow();
 
 
 
 
 // Constructor function
 var ViewModel = function() {
+    var self = this;
     this.points = ko.observableArray([
         new fMarker('Ikko Sushi', 38.99119779999999, -77.02930119999999, 'restaurant', 'WOW!I am a Japanese restaurant'),
-        new fMarker('Kung Fu Tea', 38.9970677, -77.0269637, 'drink', 'WOW!I am a Greek restaurant'),
-        new fMarker('Olazzo italian', 38.9926756, -77.02641129999999, 'restaurant', 'WOW!I am a Italian restaurant')
+        new fMarker('Kung Fu Tea', 38.9970677, -77.0269637, 'drink', 'WOW!I am a Bubble Tea.'),
+        new fMarker('Olazzo italian', 38.9926756, -77.02641129999999, 'restaurant', 'WOW!I am a Italian restaurant'),
+        new fMarker('Bump n Grind', 38.990391, -77.029788, 'drink', 'Want some coffe?'),
+        new fMarker('Whole Foods Market', 38.996330, -77.024240, 'grocery', 'Its always Fresh')
     ]);
+
+    ///fitler for the sidebar in html
+    this.filterOptions = [{
+        label: "All",
+        value: 'all'
+    }, {
+        label: "Restaurants",
+        value: 'restaurant'
+    }, {
+        label: "Drinks",
+        value: 'drink'
+    }, {
+        label: "Grocery",
+        value: 'grocery'
+    }];
 
     this.typeToShow = ko.observable('all');
 
+    this.clickOption = function(option) {
+        self.typeToShow(option.value);
+
+    }
+
+    ///filter marker with checked: typeToShow
     this.markersToShow = ko.computed(function() {
         var desiredType = this.typeToShow();
         if (desiredType == 'all') {
@@ -71,9 +97,11 @@ var ViewModel = function() {
 
     }, this);
 
-
 };
 
+/////////////////////////////////////////////////////////////
+
+// hamburger nav with slideDown and slideUp
 $(document).ready(function() {
     $(".dropdown").hover(
         function() {
@@ -115,24 +143,154 @@ $(document).ready(function() {
     });
 });
 
+
+///////// Drawing map style
+
+var styles = [{
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#f5f5f5"
+    }]
+}, {
+    "elementType": "labels.icon",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#616161"
+    }]
+}, {
+    "elementType": "labels.text.stroke",
+    "stylers": [{
+        "color": "#f5f5f5"
+    }]
+}, {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#bdbdbd"
+    }]
+}, {
+    "featureType": "poi",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#eeeeee"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#757575"
+    }]
+}, {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#e5e5e5"
+    }]
+}, {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}, {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#ffffff"
+    }]
+}, {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#757575"
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#dadada"
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#616161"
+    }]
+}, {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}, {
+    "featureType": "transit",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#e5e5e5"
+    }]
+}, {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#eeeeee"
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#c9c9c9"
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#9e9e9e"
+    }]
+}];
+
+
+
+/////// call map to display
+
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: new google.maps.LatLng(38.9926756, -77.02641129999999),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: styles
+});
+
+
+var infowindow = new google.maps.InfoWindow();
+
+
 var viewModel = new ViewModel();
-
-
 ko.applyBindings(viewModel);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
